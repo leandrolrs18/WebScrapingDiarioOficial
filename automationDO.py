9c84fcab06f1c4350edb036b6c2458b1e06c218b
leandrolrs18 <30415584+leandrolrs18@users.noreply.github.com>
2021-05-04 10:17:36 -0300

    Add files via upload

--- automationDO.py
+++ automationDO.py
@@ -1,6 +1,14 @@
 from selenium import webdriver
 import time
 
+def get_all_links(driver):
+    links = []
+    elements = driver.find_elements_by_tag_name('a')
+    for elem in elements:
+        href = elem.get_attribute("href")
+        links.append(href)
+    return links
+
 web = webdriver.Chrome()
 
 web.get('http://diariooficial.rn.gov.br/dei/dorn3/Search.aspx')
@@ -15,11 +23,18 @@ Submit = web.find_element_by_xpath('//*[@id="submit-busca-simples"]')
 Submit.click()
 time.sleep(70)
 
-elems = web.find_elements_by_tag_name('a')
-for elem in elems:
-    href = elem.get_attribute('href')
-    if href is not None:
-        print(href)
+links = []
+print(get_all_links(web))
+links = get_all_links(web);
+print(len(links))
+#elems = web.find_elements_by_tag_name('a')
+#for elem in elems:
+#    href = elem.get_attribute('href')
+#    if href is not None:
+#        print(href)
+
+#for i in links:
+#    print('a ' + links)
 
 #for elem in range (30,40):
 #    print(elems(elem))
